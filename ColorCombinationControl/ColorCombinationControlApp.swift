57584e460a4694fd36156e2d9dcc25998dc8b7ce
James Alan Bush <jamesalanbush1972@gmail.com>
2024-05-06 23:28:41 -0400

    Initial Commit

---
+++ ColorCombinationControl/ColorCombinationControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCombinationControlApp.swift
+//  ColorCombinationControl
+//
+//  Created by Xcode Developer on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorCombinationControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
